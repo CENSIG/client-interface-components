3cab6e5d33b4a22724de2feec78bc6124583cc80
Jean Boudet <jean.boudet@cenlr.org>
2015-08-06 12:54:22 +0200

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require("webpack"),
+		path    = require("path");
+
+var paths = {
+	src: path.resolve(__dirname, "public/js/"),
+	modules: path.resolve(__dirname, "node_modules/"),
+	dist: path.resolve(__dirname, "public/dist/")
+};
+
+var config = {
+	context: paths.src,
+	entry: "./index.jsx",
+	output: {
+		path: paths.dist,	
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/, 
+				exclude: /node_modules/, 
+				loader: "jsx-loader!babel-loader?stage=1"
+			}	
+		]	
+	},
+	resolve: {
+		extensions: ["", ".js", ".jsx"],
+		root: paths.src,
+		modulesDirectories: ["node_modules"]
+	},
+	watch: true
+};
+
+module.exports = config;
