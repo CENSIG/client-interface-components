a0cc4b50cf3538250514c80ce14965e427469bd3
Jean Boudet <jean.boudet@cenlr.org>
2015-08-06 18:13:14 +0200

    Add data explorer

--- tests/data.js
+++ tests/data.js
@@ -4,5 +4,18 @@ export default {
 		{ cdnom: 2016, name: "brother2" },
 		{ cdnom: 2017, name: "brother3" },
 		{ cdnom: 2018, name: "brother4" }
+	],
+
+	dataExplorerComponents: [
+		{ cdnom: 2015, name: "Maculinea alcon alcon", rang: "ES"},
+		{ cdnom: 2016, name: "Maculinea arion", rang: "ES"},
+		{ cdnom: 2017, name: "Maculinea florion", rang: "ES"},
+		{ cdnom: 2018, name: "Maculinea venon", rang: "ES"}	
+	],
+
+	dataParentsComponents: [
+		{ cdnom: 3015, name: "Lepidoptera" },
+		{ cdnom: 3016, name: "Lycanidae" },
+		{ cdnom: 3017, name: "Maculinea" }
 	]
 };
