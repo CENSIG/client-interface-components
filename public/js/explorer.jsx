198ae138b80d92bf881cc9099762d5f0f86f2c91
Jean Boudet <jean.boudet@cenlr.org>
2015-08-07 12:17:00 +0200

    Add example of explorer

--- public/js/explorer.jsx
+++ public/js/explorer.jsx
@@ -1,33 +1,55 @@
 import React from "react";
 import Immutable from "immutable";
 
-import {ExploreSubTaxon} from "../../lib/exploreSubTaxon";
+import {Explorer} from "../../lib/explorer";
 import MyExplorerView from "../../tests/components/MyExplorerView";
 
 import {dataExplorerComponents, dataParentsComponents} from "../../tests/data";
 
 export default function run() {
-	function actionClickSub() {
-		console.log("Click sur un taxon inférieurs | Déclenchement d'une action quelconque");
-	}
-
-	function actionClickSup() {
-		console.log("Click sur un taxon suprérieur | Déclenchement d'une action quelconque");
-	}
+	
 
 	React.render(
-		<ExploreSubTaxon 
+		<Explorer 
 			firstChilds={Immutable.List()}
-			parents={Immutable.List()}
+			parents={Immutable.fromJS(dataParentsComponents)}
 		/>,
 		document.getElementById("explorerEmpty")
 	);
 
 	React.render(
-		<ExploreSubTaxon
+		<Explorer
 			firstChilds={Immutable.fromJS(dataExplorerComponents)}
 			parents={Immutable.fromJS(dataParentsComponents)}
 		/>,
 		document.getElementById("explorerDefault")
+	);
+
+	React.render(
+		<Explorer
+			firstChilds={Immutable.fromJS(dataExplorerComponents)}
+			parents={Immutable.fromJS(dataParentsComponents)}
+			withCompose={MyExplorerView}
+		/>,
+		document.getElementById("explorerCompose")
+	);
+
+	const actionClickSub = () => {
+		console.log("Click sur un taxon inférieurs | Déclenchement d'une action quelconque");
+	};
+
+	const actionClickSup = () => {
+		console.log("Click sur un taxon suprérieur | Déclenchement d'une action quelconque");
+	};
+
+	React.render(
+		<Explorer
+			firstChilds={Immutable.fromJS(dataExplorerComponents)}
+			parents={Immutable.fromJS(dataParentsComponents)}
+			withCompose={MyExplorerView}
+			actionClickSup={actionClickSup}
+			actionClickSub={actionClickSub}
+		/>,
+		document.getElementById("explorerComposeCallBack")
 	);
 }
