186748a728cdaf59158d606c56a02b0720d8a7e8
Jean Boudet <jean.boudet@cenlr.org>
2015-08-10 14:25:16 +0200

    Example for search component

---
+++ public/js/search.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Immutable from "immutable";
+
+import {Search} from "../../lib/search";
+
+import MyResultItemContent from "../../tests/components/MyResultItemContent";
+
+var results = Immutable.List(); 
+var data = [
+	{ cdnom: 2016, name: "Maculinea", cdref: 2016, nameRef: "Maculinea", isref:true },
+	{ cdnom: 2017, name: "Maculinea alcon", cdref: 2017, nameRef: "Maculinea alcon", isref:true },
+	{ cdnom: 2018, name: "Maculinea alcon alcon", cdref: 2018, nameRef: "Maculinea alcon alcon", isref: true},
+	{ cdnom: 2019, name: "Maculinea faucon", cdref: 2017, nameRef: "Maculinea alcon", isref: false}
+];
+
+var notResults = null;
+
+export default function run() {
+
+	class BasicSearch extends React.Component
+	{
+		constructor(props) {
+			super(props);	
+			this.state = {
+				results: results,
+				notResults: notResults
+			};
+		}	
+
+		actionSearch(q) {
+			let r = data.filter(value => {
+				return value.name.indexOf(q) > -1;
+			});
+
+			if (r.length === 0) {
+				this.setState({ notResults: q });
+			}
+
+			this.setState({ results: Immutable.fromJS(r) });
+		}
+
+		actionReset() {
+			this.setState({
+				results: Immutable.List(),
+				notResults: null
+			});	
+		}
+
+		render() {
+			return (
+				<Search 
+					label="papillons"
+					actionSearch={this.actionSearch.bind(this)}
+					actionReset={this.actionReset.bind(this)}
+					results={this.state.results}
+					notResults={this.state.notResults}
+					header={[
+						"Taxon référent",
+						"Nom"
+					]}
+					withCompose={this.props.withCompose}
+				/>
+			);	
+		}
+	}
+
+	React.render(
+		<BasicSearch />,
+		document.getElementById("searchDefault")
+	);	
+
+	React.render(
+		<BasicSearch withCompose={MyResultItemContent} />,
+		document.getElementById("searchCompose")
+	);
+}
