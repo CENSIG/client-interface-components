7eb811a4b9aa36f30723c40a620ad9fbcf7fca3b
Jean Boudet <jean.boudet@cenlr.org>
2015-08-11 10:15:21 +0200

    Update render

--- tests/components/MyItemBrothers.jsx
+++ tests/components/MyItemBrothers.jsx
@@ -6,29 +6,12 @@ class MyItemBrothers extends React.Component
 		super(props);	
 	}
 
-	_items(props) {
-		let after = props.right 
-			? <span style={props.ArrowStyle}></span>
-			: <strong><span>{props.children}</span></strong>;
-
-		let before = props.right
-			? <strong><span>{props.children}</span></strong>
-			: <span style={props.ArrowStyle}></span>;
-
-		return (
-			<div>
-				{before}
-				{after}
-			</div>
-		);	
-	}
-
 	render() {
 		let props = this.props;
 		return (
-			<li	style={props.liStyle}>
-				{this._items(props)}
-			</li>
+			<strong>
+				{props.children}
+			</strong>
 		)	
 	}
 }
