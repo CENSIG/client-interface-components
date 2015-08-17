ace2d5ed9cf6a29a29eaf542aea524533fef850b
Jean Boudet <jean.boudet@cenlr.org>
2015-08-17 12:08:41 +0200

    Add example

--- public/js/ariane.jsx
+++ public/js/ariane.jsx
@@ -6,6 +6,15 @@ import {dataParentsComponents} from "../../tests/data";
 import MyArianeItem from "../../tests/components/MyArianeItem";
 
 export default function run() {
+	React.render( 
+		<Ariane 
+			parents={Immutable.fromJS(dataParentsComponents)} 
+			styleArianeItem={{cursor: "pointer"}}
+			styleDivBase={{border: "1px solid #eee", width: "50%"}}
+		/>,
+		document.getElementById("arianeDefault")
+	);
+
 	React.render(
 		<Ariane parents={Immutable.fromJS(dataParentsComponents)} withCompose={MyArianeItem} />,
 		document.getElementById("arianeCompose")
